6cbaa70fb022b35040c2b8e0f3a80d95b28deffb
Rain <ryanalacombe@gmail.com>
2020-08-18 16:33:42 -0500

    Created the PhysicsObject class

---
+++ PhysicsScene/PhysicsScene.cpp
@@ -0,0 +1,17 @@
+#include "PhysicsScene.h"
+
+PhysicsScene::PhysicsScene()
+{
+	m_timeStep = 0.01f;
+	m_gravity = 0;
+}
+
+void PhysicsScene::addActor(PhysicsObject* actor)
+{
+
+}
+
+void PhysicsScene::removeActor(PhysicsObject* actor)
+{
+
+}
